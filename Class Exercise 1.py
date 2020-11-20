4899b33e350108e7829831a8c38c4067ffe18fc1
Ashur Motlagh <48599194+AshurMotlagh@users.noreply.github.com>
2020-11-19 16:22:47 -0800

    Initial commit

---
+++ Class Exercise 1.py
@@ -0,0 +1,32 @@
+import math
+from math import *
+
+A = 18319910
+B = A / 9
+
+sin(A)  # 2
+1/tan(A)  # 4
+log10(A)  # 6
+log(A, 7)  # 8
+B**(-7)  # 10
+
+(abs(-1)**B) * B  # 12
+math.ceil(B)  # 14
+math.floor(B)  # 16
+max(A, B, 9)  # 18
+min(A, B, 9)  # 20
+
+  # 22
+  # 24
+  # 26
+  # 28
+  # 30
+
+
+
+
+
+
+
+
+
