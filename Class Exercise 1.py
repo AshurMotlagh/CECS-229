807a670b43a913582322b71b5489931f690c1882
Ashur Motlagh <48599194+AshurMotlagh@users.noreply.github.com>
2020-11-20 21:01:53 -0800

    Finished Class Exercise 1.py

--- Class Exercise 1.py
+++ Class Exercise 1.py
@@ -1,57 +1,66 @@
+# Ashur Motlagh
+# 018319910
+
 import math
 from math import *
 
 A = 18319910
-B = A / 9
-firstprimelist = []
-firstprimelist2 = []
+B = A % 9
+C = A / 9
+primelist = []
+extraCreditPrimeNumbers = []
 
-sin(A)  # 2
+print(sin(A))  # 2
 
-1/tan(A)  # 4 cos(A)/sin(A)
+print(1/tan(A))  # 4 cos(A)/sin(A)
 
-log10(A)  # 6
+print(log10(A))  # 6
 
-log(A, 7)  # 8
+print(log(A, 7))  # 8
 
-B**(-7)  # 10
+print(B**(-7))  # 10
 
-(abs(-1)**B) * B  # 12
+print((abs(-1)**B) * B)  # 12
 
-math.ceil(B)  # 14
+print(math.ceil(C))  # 14
 
-math.floor(B)  # 16
+print(math.floor(C))  # 16
 
-max(A, B, 9)  # 18
+print(max(A, B, 9))  # 18
 
-min(A, B, 9)  # 20
+print(min(A, B, 9))  # 20
 
-A**(1/5)  # 22
+print(A**(1/5))  # 22
 
-A/B  # 24
+print(A/B)  # 24
 
-for i in range(0,round(5*B)+1):  # 26
+for i in range(int(B), int(5 * B)):
     if i > 1:
-        for i in range(2, i):
-            if (i % i) == 0:
+        for j in range(2,i):
+            if (i % j) == 0:
                 break
         else:
-            firstprimelist.append(i)
+            primelist.append(i)
+print(primelist)
+
 
 if(((-1)**B) == -1): # 28
-    True
+    print("this is true")
 else:
-    False
+    print("this is false")
 
 name = input("What is your name: ")  # 30
 id = int(input("What is your ID: "))
 print("This is your name ", name, "and your id: ", id)
 
 # The Extra point
-for i in range(round(B),round(5*B)+1):
+num = int(input("Enter a number: "))
+for i in range(num, 5 * num):
     if i > 1:
-        for i in range(2, i):
-            if (i % i) == 0:
+        for j in range(2,i):
+            if (i % j) == 0:
                 break
         else:
-            firstprimelist2.append(i)
+            extraCreditPrimeNumbers.append(i)
+print(extraCreditPrimeNumbers)
+print(len(extraCreditPrimeNumbers))
