1a0df31d3c8fc4814166e3d2e196ce2025fde92b
Ashur Motlagh <48599194+AshurMotlagh@users.noreply.github.com>
2020-12-05 23:45:05 -0800

    Updated Class Exercise 1.py

--- Class Exercise 2.py
+++ Class Exercise 2.py
@@ -1,3 +1,25 @@
 import numpy
+
 print("remainder of id over 3 is: ", 18319910 % 3)
-matrix =
+matrix = numpy.array(
+    [[12, 5, 3, 74, 52], [23, 41, 12, 41, 23], [17, 36, 9, 8, 11], [19, 23, 14, 9, 11], [8, 2, 29, 37, 28]])
+
+print(numpy.dot(12, matrix))  # A
+
+print(numpy.linalg.det(matrix))  # B
+
+print(numpy.linalg.inv(matrix))  # C
+
+print(matrix.T)  # D
+
+print(matrix.trace())  # E
+
+print(numpy.dot(matrix, numpy.linalg.inv(matrix)))  # F
+
+print(numpy.dot(matrix,matrix))  # G
+
+print(numpy.subtract(matrix, numpy.linalg.inv(matrix)))  # H
+
+
+print(numpy.linalg.eigvals(matrix))  # J
+print(numpy.linalg.eig(matrix))  # J
