399407b14144ee57060981abc7e3e3e692ac3b35
Ashur Motlagh <48599194+AshurMotlagh@users.noreply.github.com>
2020-12-06 16:26:31 -0800

    Reformatted output with questions

--- Class Exercise 2.py
+++ Class Exercise 2.py
@@ -4,37 +4,37 @@
 
 import numpy
 
-print("remainder of id over 3 is: ", 18319910 % 3)
+print("remainder of id over 3 is: ", 18319910 % 3, "\n")
 matrix = numpy.array(
     [[12, 5, 3, 74, 52], [23, 41, 12, 41, 23], [17, 36, 9, 8, 11], [19, 23, 14, 9, 11], [8, 2, 29, 37, 28]])
 birthdayMat = numpy.array([[11, 32, 26, 28, 17]])
 
-print("12 times matrix: ", numpy.dot(12, matrix))  # A
+print("12 times matrix: ", numpy.dot(12, matrix), "\n")  # A
 
-print("Det of matrix: ", numpy.linalg.det(matrix))  # B
+print("Det of matrix: ", numpy.linalg.det(matrix), "\n")  # B
 
-print("Inverse of matrix: ",numpy.linalg.inv(matrix))  # C
+print("Inverse of matrix: ",numpy.linalg.inv(matrix), "\n")  # C
 
-print("Transpose of matrix: ", matrix.T)  # D
+print("Transpose of matrix: ", matrix.T, "\n")  # D
 
-print("Trace of matrix: ", matrix.trace())  # E
+print("Trace of matrix: ", matrix.trace(), "\n")  # E
 
-print("Multiply matrix with inverse: ", numpy.dot(matrix, numpy.linalg.inv(matrix)))  # F
+print("Multiply matrix with inverse: ", numpy.dot(matrix, numpy.linalg.inv(matrix)), "\n")  # F
 
-print("Multiply matrix with matrix: ", numpy.dot(matrix,matrix))  # G
+print("Multiply matrix with matrix: ", numpy.dot(matrix,matrix), "\n")  # G
 
-print("Matrix minus inverse: ", numpy.subtract(matrix, numpy.linalg.inv(matrix)))  # H
+print("Matrix minus inverse: ", numpy.subtract(matrix, numpy.linalg.inv(matrix)), "\n")  # H
 
 eig = numpy.linalg.eig(matrix)  # I
-print("Eigan Value: ", eig[0])
-print("Eigan Vector: ", eig[1])
+print("Eigan Value: ", eig[0], "\n")
+print("Eigan Vector: ", eig[1], "\n")
 
-print("Second row with month of birth: ", matrix[1] * 9)  # J
+print("Second row with month of birth: ", matrix[1] * 9, "\n")  # J
 
-print("Thrid column with month of birth: ", matrix[:, 2] * 9)  # K
+print("Thrid column with month of birth: ", matrix[:, 2] * 9, "\n")  # K
 
 secRow = matrix[1] * 2  # L
-print("Add first row with seconds row then replace: ", numpy.add(matrix[0],secRow))
+print("Add first row with seconds row then replace: ", numpy.add(matrix[0],secRow), "\n")
 
-print("Solve equation Ax=B: ", numpy.multiply((numpy.linalg.inv(matrix)), birthdayMat))  # M
+print("Solve equation Ax=B: ", numpy.multiply((numpy.linalg.inv(matrix)), birthdayMat), "\n")  # M
 
